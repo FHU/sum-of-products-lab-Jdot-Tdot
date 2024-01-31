75a337e0fbcfcfcb5847e3487210745bc0d2e372
Jdot-Tdot <117394245+Jdot-Tdot@users.noreply.github.com>
2024-01-31 13:45:10 -0600

    made a new function for list comprehension

--- main.py
+++ main.py
@@ -3,6 +3,18 @@
 def sum_of_products(list1, list2):
     pass
 
+def get_input_to_list():
+    the_input = input().split()
+    listing = True
+    while(listing):
+        try:
+            input_ready = [int(number) for number in the_input]
+            listing = False
+        except TypeError:
+            print("Try again with only numbers and letters")
+            the_input = input().split()
+    return input_ready
+
 if __name__ == '__main__':
    #REMOVE PASS AND YOUR CODE GOES HERE
     pass
