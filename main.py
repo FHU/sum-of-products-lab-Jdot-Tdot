4fbfb110237a91121a2434be70e2328257d0104b
Jdot-Tdot <117394245+Jdot-Tdot@users.noreply.github.com>
2024-01-31 13:52:42 -0600

    added comments

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 def sum_of_products(list1, list2):
     pass
 
+# Uses list comprehension to check for errors and returns a list with integers
 def get_input_to_list():
     the_input = input().split()
     listing = True
@@ -15,6 +16,9 @@ def get_input_to_list():
             the_input = input().split()
     return input_ready
 
+
+# Remind students that this makes it to where it only happens if it is not a module.
+# It's how the entire thing is tested.
 if __name__ == '__main__':
    #REMOVE PASS AND YOUR CODE GOES HERE
     
